ba65ad3d1907cb0e73bb8d4fa7249b231dff2215
Ashish Joshi <97600247+AJTITAN@users.noreply.github.com>
2023-04-05 17:03:50 +0000

    using o(n)

--- week 4/3.cpp
+++ week 4/3.cpp
@@ -1,9 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
+void count_sort(int arr[],int n){
+    int maxi = 0;
+    for(int i=0;i<n;i++){
+        maxi = max(maxi,arr[i]);
+    }
+    // cout<<maxi<<endl;
+    int temp[maxi];
+    for(int i=0;i<=maxi;i++){
+        temp[i] = 0;
+    }
+    for(int i=0;i<n;i++){
+        temp[arr[i]]++;
+    }
+    int j=0;
+    for(int i=0;i<=maxi;i++){
+        while(temp[i]!=0){
+            arr[j++] = i;
+            temp[i]--;
+        }
+     }
+    // for(int i=0;i<n;i++){
+    //     cout<<arr[i]<<" ";
+    // }
+}
 int main()
 {
     int n;
-    multiset<int>s;
     cout<<"enter the size of array = ";
     cin>>n;
     int arr[n];
@@ -11,16 +34,11 @@ int main()
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    for(int i=0;i<n;i++){
-        s.insert(arr[i]);
-    }
-    int j=0;
-    for(auto i:s){
-        arr[j++] = i;
-    }
+    count_sort(arr,n);
     int k;
     cout<<"enter the kth element index =" ;
     cin>>k;
-    cout<<arr[k-1];
+    cout<<"kth smallest element = "<<arr[k-1]<<endl;
+    cout<<"kth largest element = "<<arr[n-k];
     return 0;
 }
